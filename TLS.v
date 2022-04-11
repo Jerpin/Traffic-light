cea3f0759eed634770661b0798c5fdcc55b83633
Jerpin <57026482+Jerpin@users.noreply.github.com>
2022-04-11 23:06:43 +0800

    Add files via upload

--- TLS.v
+++ TLS.v
@@ -7,114 +7,110 @@ input           Jump;
 input     [3:0] Gin;
 input     [3:0] Yin;
 input     [3:0] Rin;
-output reg      Gout;
-output reg      Yout;
-output reg      Rout;
+output reg         Gout;
+output reg         Yout;
+output reg         Rout;
 
-reg [3:0] count,next_count;
-reg change;
-reg [1:0] state,next_state;
-reg [3:0] G_duration,Y_duration,R_duration;
-
-//next state logic
-always @(*) begin
-  case (state)
-    2'd0: begin
-      
-    end
-    
-    2'd1:begin //G
-      if (count == G_duration-4'd1) begin
-        next_state = state + 2'd1;// G to Y
-        next_count = 4'd0;
-      end
-    
-      else begin
-        next_state = state;
-        next_count = count + 4'd1;
-      end
-    end
-    
-    2'd2:begin //Y
-      if (count == Y_duration-4'd1) begin
-        next_state = state + 2'd1;//Y to R
-        next_count = 4'd0;
-      end
-      else begin
-        next_state = state;
-        next_count = count + 4'd1;
-      end
-    end
-    
-    2'd3:begin //R
-      if (count == R_duration-4'd1) begin
-        next_state = 2'd1;
-        next_count = 4'd0;
-      end
-      else begin
-        next_state = state;
-        next_count = count + 4'd1;
-      end
-    end
-  endcase
-end
-  
-//output logic
-always@(state) begin
-  Gout = 1'b0; 
-  Yout = 1'b0; 
-  Rout = 1'b0;
-  case(state)
-    2'd1: begin //G
-      Gout = 1'b1; 
-    end
-    
-    2'd2:begin //Y
-      Yout = 1'b1; 
-    end
-    
-    2'd3:begin //R
-      Rout = 1'b1;
-    end
-  endcase
-end 
+reg [1:0] state, next_state;
+reg [1:0] state_g = 2'd0;
+reg [1:0] state_y = 2'd1;
+reg [1:0] state_r = 2'd2;
+reg [3:0] g_duration, y_duration, r_duration, count, next_count;
 
 //state register
 always@(posedge clk) begin
   if (Set) begin
-    next_state = 2'd1; //Green
-    next_count = 4'd0; //zero
-    state = next_state;
+    next_count = 0;
+    next_state = state_g;
+    g_duration = Gin;
+    y_duration  =Yin;
+    r_duration = Rin;  
     count = next_count;
-    G_duration = Gin; //get value
-    Y_duration = Yin;
-    R_duration = Rin;
-  end
-  
-  else if (reset) begin
-    next_state = 2'd1; //green
-    next_count = 4'd0; //zero
     state = next_state;
-    count = next_count;
-  end
-  
-  else if (Jump) begin
-    next_state = 2'd3; //red
-    next_count = 4'd0; 
-    state = next_state;
-    count = next_count;
   end
   
   else if (Stop) begin
-    //state, count stays the same(stop)
+    //count = couint;
+    //state = state;
   end
-    
-  else begin
-    state = next_state;
+  
+  else if (Jump) begin
+    next_count = 0;
+    next_state = state_r; 
     count = next_count;
+    state = next_state;
+  end
+  
+  else begin 
+    count = next_count;
+    state = next_state;
   end
 
 end
-  
 
+/* cannot write at here since it will not wait for the clock, it should change in state register
+//count
+always@(*)begin
+  count = next_count;
+  state = next_state;
+end
+*/
+
+//next state logic
+always@(*) begin
+  case(state)
+    state_g: begin //green
+      if (count == g_duration-1) begin
+        next_state = state_y;
+        next_count = 4'd0;
+      end
+      else begin
+        next_state = state;
+        next_count = count+4'd1;
+      end
+    end
+    
+    state_y: begin //yellow
+      if (count == y_duration-1) begin
+        next_state = state_r;
+        next_count = 4'd0;
+      end
+      else begin
+        next_state = state;
+        next_count = count+4'd1;
+      end
+    end
+    
+    state_r: begin //red
+      if (count == r_duration-1) begin
+        next_state = state_g;
+        next_count = 4'd0;
+      end
+      else begin
+        next_state = state;
+        next_count = count+4'd1;
+      end
+    end
+  endcase 
+end
+
+
+//output logic
+always@(state) begin
+  case(state) 
+    state_g: begin
+      Gout = 1'b1; Yout = 1'b0; Rout = 1'b0;
+    end
+    
+    state_y:begin
+      Gout = 1'b0; Yout = 1'b1; Rout = 1'b0;
+    end
+    
+    state_r:begin
+      Gout = 1'b0; Yout =1'b0; Rout = 1'b1;
+    end
+  endcase
+end      
+  
+  
 endmodule
